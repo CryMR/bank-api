905be5f9fd269923559d2a0ef9f3e4cce37cb558
fsociety . <vlad.bessmertnyy@gmail.com>
2026-02-25 21:30:49 +0100

    fixed database_url recognizing by database.py

--- database.py
+++ database.py
@@ -6,12 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-
-#URL: postgresql://user:password@postgresserver/db_name
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@db:5432/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
